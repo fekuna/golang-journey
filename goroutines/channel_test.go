5d37d2736279f02a40b38a9fc7e48100f95e40ae
Alfan <alfan.almunawar@yummycorp.com>
2022-12-28 18:12:53 +0700

    channel select and range

--- goroutines/channel_test.go
+++ goroutines/channel_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -61,7 +62,7 @@ func TestInOutChannel(t *testing.T) {
 	time.Sleep(5 * time.Second)
 }
 
-// Buffered channel
+// --- Buffered channel
 func TestBufferedChannel(t *testing.T) {
 	channel := make(chan string, 1)
 
@@ -79,4 +80,52 @@ func TestBufferedChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("Selesai")
+}
+
+// --- RANGE CHANNEL
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
+	fmt.Println("Selesai")
+}
+
+//  --- SELECT CHANNEL
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Sedang Menunggu..")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+
 }
