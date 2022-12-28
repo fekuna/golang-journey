ed8d8919ba48c2bdd687dc9b05bf8b23c17838f0
Alfan <alfan.almunawar@yummycorp.com>
2022-12-28 13:53:07 +0700

    in out channel

--- goroutines/channel_test.go
+++ goroutines/channel_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestCreateChannel(t *testing.T) {
@@ -35,4 +36,27 @@ func TestChannelAsParameter(t *testing.T) {
 	data := <-channel
 
 	fmt.Println("TestChannelAsParameter", data)
+}
+
+// -- IN OUT CHANNEL
+// Cahnnel Only Receive data
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Alfan Almunawar"
+}
+
+// Channel Only Send data
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
 }
